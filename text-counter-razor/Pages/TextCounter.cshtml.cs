aae44b42c9dd621b5c1c8a072221f0b6c76ee552
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-02-12 15:43:25 -0600

    Add comments

--- text-counter-razor/Pages/TextCounter.cshtml.cs
+++ text-counter-razor/Pages/TextCounter.cshtml.cs
@@ -4,14 +4,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class TextCounterModel : PageModel
 {
     [BindProperty]
-    public string textContent { get; set; }
-    public int characters { get; set; }
-    public int words { get; set; }
-    public int lines { get; set; }
+    public string textContent { get; set; }//text content string input from text box
+    public int characters { get; set; }//number of characters
+    public int words { get; set; }//number of words
+    public int lines { get; set; }//number of lines
     public void OnPost()
     {
-        textContent = Request.Form["textContent"];
-        characters = textContent.Length;
+        textContent = Request.Form["textContent"];//get text box input 
+        characters = textContent.Length;//update number of characters, words and lines based on entered text
         words = textContent.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
         lines = textContent.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length;
     }
