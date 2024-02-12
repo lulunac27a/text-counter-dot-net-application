0e265db2cca9ae52f94a28dcda9bb20b198ead43
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-02-12 15:54:59 -0600

    Fix warnings

--- text-counter-razor/Pages/TextCounter.cshtml.cs
+++ text-counter-razor/Pages/TextCounter.cshtml.cs
@@ -4,15 +4,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class TextCounterModel : PageModel
 {
     [BindProperty]
-    public string textContent { get; set; }//text content string input from text box
+    public string? textContent { get; set; }//text content string input from text box
     public int characters { get; set; }//number of characters
     public int words { get; set; }//number of words
     public int lines { get; set; }//number of lines
     public void OnPost()
     {
         textContent = Request.Form["textContent"];//get text box input 
-        characters = textContent.Length;//update number of characters, words and lines based on entered text
-        words = textContent.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
-        lines = textContent.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length;
+        characters = textContent?.Length ?? 0;//update number of characters, words and lines based on entered text
+        words = textContent?.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length ?? 0;
+        lines = textContent?.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length ?? 0;
     }
 }
