6f85689ca91108c91c6d7a8650df2f35dc7e6ffe
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-02-21 21:34:22 -0600

    Add ASP.NET text counter console application using C#

---
+++ text-counter-console-app/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Type text, then press Ctrl+D to calculate the number of characters, words and lines.");
+string textContent = "";
+while (true)
+{
+    string textInput = Console.ReadLine();//read text input in each line
+    if (textInput == null | textInput == "")//if Ctrl+D is pressed or text input is empty
+    {
+        break;//end while loop
+    }
+    textContent += textInput + '\n';//add text content from entered text input for each line
+}
+Console.WriteLine($"Characters: {textContent.Length}");//number of characters
+Console.WriteLine($"Words: {textContent.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length}");//number of words
+Console.WriteLine($"Lines: {textContent.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length}");//number of lines
