7f783090fb033fe5235ad6d22344b373eead958e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-11-07 18:26:51 -0600

    Format C# code

--- text-counter-console-app/Program.cs
+++ text-counter-console-app/Program.cs
@@ -1,14 +1,20 @@
-﻿Console.WriteLine("Type text, then press Ctrl+D to calculate the number of characters, words and lines.");
+﻿Console.WriteLine(
+    "Type text, then press Ctrl+D to calculate the number of characters, words and lines."
+);
 string textContent = "";
 while (true)
 {
-    string textInput = Console.ReadLine();//read text input in each line
-    if (textInput == null | textInput == "")//if Ctrl+D is pressed or text input is empty
+    string textInput = Console.ReadLine(); //read text input in each line
+    if (textInput == null | textInput == "") //if Ctrl+D is pressed or text input is empty
     {
-        break;//end while loop
+        break; //end while loop
     }
-    textContent += textInput + '\n';//add text content from entered text input for each line
+    textContent += textInput + '\n'; //add text content from entered text input for each line
 }
-Console.WriteLine($"Characters: {textContent.Length.ToString("N0")}");//number of characters
-Console.WriteLine($"Words: {textContent.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length.ToString("N0")}");//number of words
-Console.WriteLine($"Lines: {textContent.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length.ToString("N0")}");//number of lines
+Console.WriteLine($"Characters: {textContent.Length.ToString("N0")}"); //number of characters
+Console.WriteLine(
+    $"Words: {textContent.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length.ToString("N0")}"
+); //number of words
+Console.WriteLine(
+    $"Lines: {textContent.Split('\n', StringSplitOptions.RemoveEmptyEntries).Length.ToString("N0")}"
+); //number of lines
